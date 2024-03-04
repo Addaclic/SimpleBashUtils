0d555a80d08519301c8c78031dcb6b6f711d5b42
NOIDOLru <andreylab@bk.ru>
2024-03-04 10:53:22 +0700

    some_comments_cat

--- My_cat/My_cat.h
+++ My_cat/My_cat.h
@@ -1,7 +1,11 @@
 /**
- * \mainpage The **cat utility** is used to concatenate and display the contents
- *           of one or more files.
- *
+ * \mainpage The **cat utility**
+ * \file
+ * \brief This utilitie is used to concatenate and display the contents
+ *        of one or more files andprovide powerful text manipulation
+ *        capabilities in the bash shell
+ * \version 0.1
+ * \todo Tests & Bugfix
  * \author Andrey addaclic
  * \copyright GNU Public License
  */
@@ -32,23 +36,23 @@ typedef struct {
 
 /**
  * @brief Function for parsing options from the command line
- * @param int first argument of the function argc
- * @param char_ptr second argument of the function argv
- * @param my_cat_opt third argument of the function that contains options
- * @return a condition of success or failure
+ * @param[in] argc first argument of the function count of arguments
+ * @param[in] argv second argument of the function lists of names of arguments
+ * @param[out] my_cat_opt third argument of the function that contains options
+ * @return a condition of success or failure of parsing
  */
 int parse_opts(int, char *[], my_cat_opt *);
 
 /**
- * @brief Read and print file
- * @param int first argument of the function argc
- * @param char_ptr second argument of the function argv
- * @param my_cat_opt third argument of the function that contains options
+ * @brief Read and print file in commond line
+ * @param[in] argc first argument of the function "argc"
+ * @param[in] argv second argument of the function "argv"
+ * @param[in] my_cat_opt third argument of the function that contains options
  */
 void output_file(int, char *[], my_cat_opt);
 
 /**
- * @brief Print text with options
+ * @brief Print text with given options
  * @param FILE_ptr first argument of the function that contains file data
  * @param my_cat_opt second argument of the function that contains options
  */
